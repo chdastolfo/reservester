c2599858c1f976d7f80527bfb6e78b88c3e62298
Christina D'Astolfo <chdastolfo@gmail.com>
2016-07-09 19:54:32 -0400

    everything is REALLY fixed and nothing is broken - added user authentication

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,7 @@
 class RestaurantsController < ApplicationController
 	
 	before_action :authenticate_owner!, except: [:show, :index]
-	before_action :set_restaurants, only: [:show]
+	before_action :set_restaurant, only: [:show]
 
 def index
 	@restaurants = Restaurant.all
@@ -12,37 +12,37 @@ def new
 end
 
 def create
-	@restaurant = Restaurant.new(restaurant_params)
+	@restaurants = Restaurant.new(restaurant_params)
 	@restaurants = current_owner.restaurants.new(restaurant_params)
 	if @restaurants.save
 		flash[:success] = "Restaurant was created, bitch."
-		redirect_to @restaurant
+		redirect_to @restaurants
 	else
 		render 'new'
 	end
 end
 
 def show
-	#@restaurants = Restaurant.find(params[:id])
+	#@restaurant = Restaurant.find(params[:id])
 end
 
 def edit
-	@restaurent = current_owner.restaurants.find(params[:id])
+	@restaurants = current_owner.restaurants.find(params[:id])
 end
 
 def update
 	@restaurants = current_owner.restaurants.find(params[:id])
 	if @restaurants.update(restaurant_params)
 		flash[:success] = "Restaurant was updated, bitch."
-		redirect_to @restaurant
+		redirect_to @restaurants
 	else
 		render 'edit'
 	end
 end
 
 def destroy
-	@restaurant = current_owner.restaurants.find(params[:id])
-	if @restaurant.destroy
+	@restaurants = current_owner.restaurants.find(params[:id])
+	if @restaurants.destroy
 		flash[:danger] = "restaurant was deleted and stuff"
 	else
 		flash[:danger] = "restaurant was not deleted and stuff"
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -4,5 +4,8 @@ class Owner < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+validates :email, presence: true, uniqueness: true
+validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create 
+
    has_many :restaurants
 end
